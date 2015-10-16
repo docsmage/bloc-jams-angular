388e2154524a961dc3daab6fa239e50fd589c1c7
Renee CS <voiceofrae@gmail.com>
2015-10-15 21:19:50 -0700

    checkpoint lesson complete

---
+++ app/scripts/app.js
@@ -0,0 +1,3 @@
+var blocJamsModule = angular.module('blocJams', []);
+//blocJamsModule is the name of the identifier assigned to the module
+//blocJams is the name of the module and is the link between the apps HTML and the module
