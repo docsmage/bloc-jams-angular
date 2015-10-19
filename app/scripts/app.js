192b01be80a78f3b79e62b7de56aeecdedd8a7a4
Renee CS <voiceofrae@gmail.com>
2015-10-19 15:45:54 -0700

    checkpoint assignment complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,34 @@
-var blocJamsModule = angular.module('blocJams', []);
+var blocJamsModule = angular.module('blocJams', ['ui.router']);
 //blocJamsModule is the name of the identifier assigned to the module
 //blocJams is the name of the module and is the link between the apps HTML and the module
+
+blocJamsModule.config(function($stateProvider, $locationProvider) {
+	
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+	
+	$stateProvider
+	
+		.state('album', {
+		url: '/album',
+		controller: 'Album.controller',
+		templateUrl: '/templates/album.html'
+	})
+	
+		.state('collection', {
+		url: '/collection',
+		controller: 'Collection.controller',
+		templateURL: '/templates/collection.html'
+	})
+	
+		.state('landing', {
+		url: '/', // where the user initially lands
+		controller: 'Landing.controller',
+		templateURL: '/templates/landing.html'
+	});
+	
+});
+// $locationProvider configures an applications paths
+// $stateProvider will configure aspects of a state including name, URL route, controller and template
